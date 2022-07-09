ac90bdfa064181f9e2ce974da212b747d032393e
Henrique Chagas <henriquechagasg@gmail.com>
2022-07-09 16:56:10 -0300

    feat: create redux store and persistor

---
+++ redux/index.ts
@@ -0,0 +1,41 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import createSecureStore from 'redux-persist-expo-securestore';
+import { api } from '../api';
+
+import authReducer from './auth';
+
+const storage = createSecureStore();
+
+const authPersistConfig = {
+  key: 'examplekey',
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    auth: persistedAuthReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (gDM) =>
+    gDM({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(api.middleware),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
