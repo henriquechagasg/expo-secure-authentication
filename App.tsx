a7e452da7eb620d7493e98a318b52fc30371cd45
Henrique Chagas <henriquechagasg@gmail.com>
2022-07-09 17:10:20 -0300

    feat: implement authentication on main screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { persistor, store } from './redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { loggedIn, loggedOut, selectUser } from './redux/auth';
+import { useAuthenticateMutation } from './api';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
   return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Auth />
+      </PersistGate>
+    </Provider>
+  );
+}
+
+function Auth() {
+  const dispatch = useDispatch();
+
+  const [authenticate, _] = useAuthenticateMutation();
+
+  const user = useSelector(selectUser);
+
+  const isLoggedIn = !!user.id;
+
+  async function handleLogin() {
+    const userData = await authenticate().unwrap();
+
+    dispatch(loggedIn(userData));
+  }
+
+  async function handleLogout() {
+    dispatch(loggedOut());
+  }
+
+  return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        User current state:
+        {isLoggedIn ? 'Authenticated' : 'Not Authetnicated'}
+      </Text>
+      <Button title="Login" onPress={handleLogin} />
+      {isLoggedIn && (
+        <>
+          <Text style={styles.text}>
+            Current user data: {JSON.stringify(user)}
+          </Text>
+          <Button title="Logout" onPress={handleLogout} />
+        </>
+      )}
     </View>
   );
 }
@@ -16,5 +59,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
+  },
+  text: {
+    textAlign: 'center',
   },
 });
